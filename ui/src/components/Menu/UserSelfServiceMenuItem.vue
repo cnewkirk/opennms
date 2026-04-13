bab466de3d275c0b75811cf3cd22f9230a3650f4
Chance Newkirk <chance.newkirk+github@pm.me>
2026-04-12 23:49:50 -0700

    refactor(shell): migrate UserSelfServiceMenuItem from Feather to PrimeVue

--- ui/src/components/Menu/UserSelfServiceMenuItem.vue
+++ ui/src/components/Menu/UserSelfServiceMenuItem.vue
@@ -1,120 +1,79 @@
 <template>
-  <FeatherDropdown
-    class="self-service-menubar-dropdown"
-    :modelValue="expanded"
-    @update:modelValue="(val: any) => updateDisplay(val)"
-  >
-    <template v-slot:trigger="{ attrs, on }">
-      <div @mouseenter="showMenu" class="self-service-menubar-icon-wrapper">
-        <FeatherButton link href="#" v-bind="attrs" v-on="on" class="self-service-menubar-dropdown-button-dark">
-          <FeatherIcon :icon="IconAccountCircle" class="self-service-top-icon" />
-          <FeatherIcon class="self-service-arrow-dropdown" :icon="ArrowDropDown" />
-        </FeatherButton>
-      </div>
-    </template>
-
-    <FeatherDropdownItem
-      @click="onUserProfileMenuClick"
+  <div class="self-service">
+    <Button
+      text
+      rounded
+      class="self-service__btn"
+      title="User menu"
+      @click="toggle"
     >
-      <div class="self-service-menubar-dropdown-item-content">
-        <a :href="computeLink('')" class="dropdown-menu-link dropdown-menu-wrapper final-menu-wrapper" name="self-service-user">
-          <FeatherIcon :icon="IconAccountCircle" class="self-service-icon" />
-          <span class="left-margin-small">
-            {{ ellipsify(mainMenu.username || '', 40) }}
-          </span>
+      <template #default>
+        <i class="pi pi-user self-service__icon" />
+        <i class="pi pi-angle-down self-service__chevron" />
+      </template>
+    </Button>
+
+    <Popover ref="popoverRef" class="self-service-popover">
+      <div class="self-service-menu">
+        <a
+          :href="computeLink('')"
+          class="self-service-menu__item self-service-menu__item--username"
+          @click.prevent="onUserProfileMenuClick"
+        >
+          <i class="pi pi-user" />
+          <span class="self-service-menu__username">{{ ellipsify(mainMenu.username || '', 40) }}</span>
+        </a>
+
+        <div class="self-service-menu__divider" />
+
+        <a
+          v-for="item in menuItems"
+          :key="item?.id || ''"
+          :href="computeLink(item?.url || '')"
+          class="self-service-menu__item"
+          @click.prevent="onMenuItemClick(item)"
+        >
+          <i :class="['pi', getItemIcon(item)]" />
+          <span>{{ item?.name || '' }}</span>
         </a>
       </div>
-    </FeatherDropdownItem>
-
-    <FeatherDropdownItem
-       v-for="item in menuItems"
-       :key="item?.id || ''"
-       @click="onMenuItemClick(item)"
-    >
-      <div class="self-service-menubar-dropdown-item-content">
-        <a :href="computeLink(item?.url || '')" class="dropdown-menu-link dropdown-menu-wrapper final-menu-wrapper" :name="`self-service-${item.id}`">
-          <FeatherIcon :icon="createIcon(item)" class="self-service-icon" />
-          <span class="left-margin-small">
-            {{ item?.name || '' }}
-          </span>
-        </a>
-      </div>
-    </FeatherDropdownItem>
-  </FeatherDropdown>
+    </Popover>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { DefineComponent } from 'vue'
-import { FeatherDropdown, FeatherDropdownItem } from '@featherds/dropdown'
-import { FeatherIcon } from '@featherds/icon'
-import ArrowDropDown from '@featherds/icon/navigation/ArrowDropDown'
-import IconAccountCircle from '@featherds/icon/action/AccountCircle'
-import IconHelp from '@featherds/icon/action/Help'
-import IconLogout from '@featherds/icon/action/LogOut'
-import IconSecurity from '@featherds/icon/network/Security'
+import Button from 'primevue/button'
+import Popover from 'primevue/popover'
 import { ellipsify } from '@/lib/utils'
 import { performLogout } from '@/services/logoutService'
 import { useMenuStore } from '@/stores/menuStore'
-import {
-  MainMenu,
-  MenuItem
-} from '@/types/mainMenu'
+import { MainMenu, MenuItem } from '@/types/mainMenu'
 
 const menuStore = useMenuStore()
 const mainMenu = computed<MainMenu>(() => menuStore.mainMenu)
 
-defineProps({
-  expanded: {
-    required: true,
-    type: Boolean
-  }
-})
+const popoverRef = ref()
+const toggle = (event: Event) => popoverRef.value?.toggle(event)
 
-const emit = defineEmits(['menu-show', 'menu-hide'])
-
-const updateDisplay = (val: any) => {
-  if (val === true) {
-    emit('menu-show')
-  } else {
-    emit('menu-hide')
-  }
-}
-
-const showMenu = () => {
-  emit('menu-show')
-}
-
-const menuItems = computed<MenuItem[]>(() => {
-  const helpMenu = mainMenu.value.helpMenu?.items?.find(m => m.id === 'helpMain')
-  const changePasswordMenu = mainMenu.value.selfServiceMenu?.items?.find(m => m.id === 'changePassword')
-  const logoutMenu = mainMenu.value.selfServiceMenu?.items?.find(m => m.id === 'logout')
-
-  return [helpMenu, changePasswordMenu, logoutMenu].map(m => m as MenuItem).filter(m => m !== undefined) || []
-})
-
-const createIcon = (menuItem: MenuItem) => {
-  let icon: DefineComponent | null = null
-
-  switch (menuItem.id) {
-    case 'helpMenu':
-      icon = IconHelp; break
-    case 'logout':
-      icon = IconLogout; break
-    case 'changePassword':
-      icon = IconSecurity; break
-  }
-
-  return (icon ?? IconHelp) as typeof FeatherIcon
-}
+const menuItems = computed<MenuItem[]>(() =>
+  mainMenu.value?.selfServiceMenu?.items ?? []
+)
 
 const computeLink = (url: string) => {
-  const baseLink = mainMenu.value?.baseHref || import.meta.env.VITE_BASE_URL || ''
-  return `${baseLink}${url}`
+  const base = mainMenu.value?.baseHref ?? '/opennms/'
+  return url ? (url.startsWith('http') ? url : `${base}${url}`) : '#'
+}
+
+const getItemIcon = (item: MenuItem): string => {
+  if (item.action === 'logout') return 'pi-sign-out'
+  if (item.id === 'selfServicePassword' || item.id === 'changePassword') return 'pi-lock'
+  if (item.id === 'apiTokens') return 'pi-key'
+  return 'pi-external-link'
 }
 
 const onUserProfileMenuClick = () => {
-  const link = computeLink('')
-  window.location.assign(link)
+  const url = mainMenu.value?.selfServiceMenu?.url ?? ''
+  if (url) window.location.assign(computeLink(url))
 }
 
 const onMenuItemClick = async (item: MenuItem) => {
@@ -122,154 +81,68 @@ const onMenuItemClick = async (item: MenuItem) => {
     await performLogout()
     return
   }
-
-  const link = computeLink(item.url || '')
-  window.location.assign(link)
+  window.location.assign(computeLink(item.url || ''))
 }
 </script>
 
 <style lang="scss" scoped>
-@import "@featherds/dropdown/scss/mixins";
-@import "@featherds/styles/mixins/elevation";
-@import "@featherds/styles/mixins/typography";
-@import "@featherds/styles/themes/variables";
+.self-service {
+  position: relative;
 
-.dropdown-menu-link {
-  color: var($primary-text-on-surface) !important;
+  &__btn {
+    color: var(--feather-primary-text-on-primary) !important;
+    display: flex;
+    align-items: center;
+    gap: 4px;
+  }
 
-  &:hover {
+  &__icon {
+    font-size: 20px;
+  }
+
+  &__chevron {
+    font-size: 12px;
+  }
+}
+
+.self-service-popover {
+  min-width: 200px;
+}
+
+.self-service-menu {
+  display: flex;
+  flex-direction: column;
+
+  &__divider {
+    height: 1px;
+    background: var(--feather-border-light-on-surface);
+    margin: 4px 0;
+  }
+
+  &__item {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    padding: 10px 14px;
+    color: var(--feather-primary-text-on-surface);
     text-decoration: none;
-  }
-}
+    font-size: 13px;
+    cursor: pointer;
 
-.self-service-menubar-dropdown {
-  margin-left: 2px;
-
-  :deep(.feather-dropdown) {
-    @include dropdown-menu-height(10);
-  }
-}
-
-.self-service-menubar-dropdown-dark {
-  margin-left: 2px;
-  &.help-menu {
-    margin-left:1px;
-  }
-  :deep(.feather-dropdown) {
-    @include dropdown-menu-height(10);
-  }
-}
-
-.self-service-menubar-dropdown-button-dark {
-  // make it look more like OG menu
-  color: rgba(255, 255, 255, 0.78);
-  background-color: transparent; // header provides the background (blue in light mode, dark in dark mode)
-  text-transform: none;
-  letter-spacing: normal;
-  font-weight: 600; // 400
-  font-size: 0.875rem;
-  padding-left: 0.5rem;
-  padding-right: 0.5rem;
-}
-
-.btn.self-service-menubar-dropdown-button-dark {
-  :deep(.btn-content) {
-    // make it look more like OG menu
-    color: rgba(255, 255, 255, 0.78);
-    text-transform: none;
-    letter-spacing: normal;
-    font-weight: 600;
-    font-size: 0.875rem;
-    padding-left: 0.1rem;
-    padding-right: 0.1rem;
-  }
-}
-
-div.self-service-menubar-icon-wrapper {
-  .self-service-menubar-dropdown-button-dark {
-    svg.self-service-top-icon.feather-icon {
-      vertical-align: -0.5rem;
+    &:hover {
+      background: rgba(0, 0, 0, 0.06);
     }
-    svg.self-service-arrow-dropdown.feather-icon {
-      vertical-align: 0;
-    }
-  }
-}
 
-// should match menubar-dropdown-item-content in UserNotificationsMenu
-.self-service-menubar-dropdown-item-content {
-  padding-top: 0.25rem;
-  padding-right: 0.5rem;
-  padding-bottom: 0.25rem;
-  padding-left: 0.5rem;
-  font-size: 0.875rem;
-  font-weight: 400;
-}
-
-.feather-icon.self-service-top-icon {
-  font-size: 2em;
-  margin-right: 0.25rem;
-}
-
-.feather-icon.self-service-icon {
-  font-size: 1.25em;
-}
-
-.self-service-menubar-dropdown-item-content.menubar-padding {
-  padding: 10px;
-}
-</style>
-
-<style lang="scss">
-@use '@/styles/vars' as vars;
-@import "@featherds/styles/themes/open-mixins";
-@import "@featherds/styles/themes/variables";
-
-body .feather-menu .feather-menu-dropdown {
-  border-radius: vars.$border-radius-surface;
-
-  .feather-dropdown {
-    border: 1px solid rgba(0, 0, 0, .35);
-  }
-}
-
-.feather-dropdown {
-  .feather-list-item {
-    height: auto;
-    padding: 0;
-  }
-
-  .dropdown-menu-wrapper {
-    padding: 0 1em;
-    min-width: 400px;
-    padding-top: 10px;
-
-    &.show-more-link {
-      padding-bottom: 10px;
-
-      a {
-        color: var(--feather-primary-text-on-surface)
-      }
+    &--username {
+      font-weight: 600;
     }
   }
 
-  .final-menu-wrapper {
-    display: block;
-    padding-left: 20px;
-    padding-bottom: 10px;
-
-    svg {
-      margin-right: 10px;
-    }
-  }
-}
-
-.feather-menu {
-  &.self-service-menubar-dropdown {
-    margin-left:0;
-  }
-  .self-service-menubar-dropdown-button-dark {
-    padding: 0 7px;
+  &__username {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    max-width: 160px;
   }
 }
 </style>
