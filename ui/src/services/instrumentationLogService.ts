a2c29201171a9221f455816802bf22742082e83f
Chance Newkirk <chance.newkirk+github@pm.me>
2026-05-01 18:38:46 -0700

    feat: add instrumentationLogService

---
+++ ui/src/services/instrumentationLogService.ts
@@ -0,0 +1,31 @@
+import { v2 } from '@/services/axiosInstances'
+
+export interface ServiceCollectorEntry {
+  serviceId: string
+  parsedServiceId: string
+  collectionCount: number
+  successfulCollectionCount: number
+  errorCollectionCount: number
+  successPercentage: number
+  errorPercentage: number
+  avgCollectionTimeMs: number
+  avgTimeBetweenCollectionsMs: number
+  avgErrorCollectionTimeMs: number
+  avgPersistTimeMs: number
+  totalPersistTimeMs: number
+}
+
+export type SortColumn = 'TOTALCOLLECTS' | 'AVGCOLLECTTIME' | 'AVGTIMEBETWEENCOLLECTS'
+  | 'TOTALSUCCESSCOLLECTS' | 'AVGSUCCESSCOLLECTTIME' | 'TOTALERRORS'
+  | 'AVGERRORTIME' | 'AVGPERSISTTIME' | 'TOTALPERSISTTIME'
+
+export type SortOrder = 'ASCENDING' | 'DESCENDING'
+
+export const getInstrumentationLog = async (params?: {
+  search?: string
+  sortColumn?: SortColumn
+  sortOrder?: SortOrder
+}): Promise<ServiceCollectorEntry[]> => {
+  const resp = await v2.get<ServiceCollectorEntry[]>('/instrumentation-log', { params })
+  return resp.data
+}
